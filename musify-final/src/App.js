f8929d764831bc2d1b7a05fd89717340c0c98ab0
gupta1123 <95712374+gupta1123@users.noreply.github.com>
2023-08-18 22:35:35 +0530

    Add files via upload

---
+++ musify-final/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import MusicClasses from "./MusicClasses";
+
+function App() {
+  return (
+    <div style={{ display: "flex" }}>
+      <Sidebar />
+      <MusicClasses />
+    </div>
+  );
+}
+
+export default App;
